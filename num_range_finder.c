5ea2fcc9ad92d3d1ae320cb2bacc0b57b0be0169
David MANZI <manzidavid093@gmail.com>
2024-07-04 16:16:07 +0300

    Initial commit

---
+++ num_range_finder.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+
+    int num;
+
+    printf("\nEnter any number: ");
+    scanf("%d", &num);
+
+    if(num >= 0 && num <= 9) {
+        printf("\nThe number provided is between 0 and 9!\n\n");
+    } else if(num >= 10 && num <= 19) {
+        printf("\nThe number provided is between 10 and 19!\n\n");
+    } else if(num >= 20 && num <= 29) {
+        printf("\nThe number provided is between 20 and 29!\n\n");
+    } else if(num >= 30 && num <= 39) {
+        printf("\nThe number provided is between 30 and 39!\n\n");
+    } else {
+        printf("\nThe number provided doesn't coresspond to any range!\n\n");
+    }
+
+    return 0;
+}
---
+++ smallest_num_finder.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    
+    int numA, numB, numC, one_of_two, smallN;
+
+    printf("\nEnter the first number: ");
+    scanf("%d", &numA);
+
+    printf("\nEnter the second number: ");
+    scanf("%d", &numB);
+
+    printf("\nEnter the third number: ");
+    scanf("%d", &numC);
+
+    one_of_two = (numA < numB)? numA : numB;
+    smallN = (one_of_two < numC)? one_of_two : numC;
+
+    printf("\nThe smallest of the three numbers is: %d\n", smallN);
+
+    return 0;
+}
